f05c1096ebc53a70acc3e475c6db721214484115
Nicolas Costa de Lima <nicolasdev4730@gmail.com>
2025-11-19 21:51:33 -0300

    Criando os arquivos pt1

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Permite o backend receber JSON
+app.use(express.json());
+
+// Libera o acesso do frontend
+app.use(cors());
+
+// Rota inicial só para testar
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando! 🔥");
+});
+
+// Iniciar servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
